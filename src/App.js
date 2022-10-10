6483650a39d5dfaf585f635a37c6d8bde4554c66
David Barba <david.barbapedreno@metronom.com>
2022-10-10 12:56:16 +0200

    testing travisci

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const displayMyMessage = () => 'This is my message'
   return (
     <div className="App">
       <header className="App-header">
@@ -9,13 +11,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{displayMyMessage()}</p>
+        <p>Thank you. We just updated.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Study React
         </a>
       </header>
     </div>
